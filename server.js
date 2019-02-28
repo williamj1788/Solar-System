97eaec7a3e6596e21e672ba86e7d453cbf3aa102
Jacquez Williams <jacquezwilliams115@gmail.com>
2019-02-28 05:39:31 -0500

    deleted unused server file

--- server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-var server = http.createServer((req,res) => {
-    console.log('Server Recieved Requested');
-})
-var PORT = 8080;
-server.listen(PORT,() => {console.log(`Server running on ${PORT}...`)});
